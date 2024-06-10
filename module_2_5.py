68829a05639c7884e361e24a93b30d86726e2348
Aleksandr <kostromin63@mail.ru>
2024-06-10 21:28:47 +0300

    Выполнение пятого заадния модуля №2

--- module_2_5.py
+++ module_2_5.py
@@ -30,7 +30,7 @@ result2 = get_matrix(3, 5, 42)
 print(result2)
 result3 = get_matrix(4, 2, 13)
 print(result3)
-
+#
 # После всех цpиклов верните значение переменной matrix.
 # Выведите на экран(консоль) результат работы функции get_matix.
 #
