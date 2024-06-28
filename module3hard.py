f9c74c2da38860b24caa92df566a3246fb455e57
Aleksandr <kostromin63@mail.ru>
2024-06-28 14:10:20 +0300

    Домашняя работа по уроку "Модули и пакеты

--- module3hard.py
+++ module3hard.py
@@ -56,3 +56,58 @@
 # Рекомендуется применить рекурсивный вызов функции, для каждой внутренней структуры.
 # Т.к. каждая структура может содержать в себе ещё несколько элементов, можно использовать параметр *args
 # Для определения типа данного используйте функцию isinstance.
+
+def calculate_structure_sum(data_structure):
+
+    # data_ = data_structure
+    structure_length = len(data_structure)
+
+    for i in data_structure:
+    #next_element = None #data_[0]
+
+
+        if isinstance(i, list):
+        #     # Разберем список
+            structure_length = len(data_structure)
+            if len(i) <= 1:
+                return next_element
+            #next_element = data_structure[0]
+            return calculate_structure_sum(data_structure[0])
+        # elif isinstance(data_structure, tuple):
+        # #     # Разберем кортеж
+        #     #next_element = data_[0]
+        #     if len(next_element) <= 1:
+        #         return next_element
+        #     return calculate_structure_sum(next_element[1:])
+        # elif isinstance(data_structure, dict):
+        # #     # Разберем
+        #     #next_element = data_[0]
+        #     if len(next_element) <= 1:
+        #         return next_element
+        #     return calculate_structure_sum(next_element[1:])
+        elif isinstance(data_structure, int):
+            next_element = data_structure
+
+            return next_element
+        print(data_)
+        print(type(data_))
+        print(next_element)
+        print(type(next_element))
+    # print(next_)
+    # print(type(next_))
+    # print(next2)
+    # print(type(next2))
+#    return 123
+    return str(next_element) + calculate_structure_sum(data_[1:])
+
+#data_structure = [[]]
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+# data_structure = 1
+result = calculate_structure_sum(data_structure)
+print(result)
---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fd
+from true_math import divide as td
+
+print('Функция fd выдает результат: ' + str(fd(5, 2)))
+print('Функция fd выдает результат: ' + str(fd(5, 0)))
+print('Функция td выдает результат: ' + str(td(55,5)))
+print('Функция td выдает результат: ' + str(td(5, 0)))
