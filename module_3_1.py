5bfe03ef1b88aa36a302a1ae77805438a646fec1
Aleksandr <kostromin63@mail.ru>
2024-07-17 11:29:36 +0300

    # Домашняя работа по уроку "Пространство имён"

--- module_3_1.py
+++ module_3_1.py
@@ -24,11 +24,7 @@
 #
 # Пример результата выполнения программы:
 # Пример выполняемого кода:
-# print(string_info('Capybara'))
-# print(string_info('Armageddon'))
-# print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-# print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-# print(calls)
+
 # Вывод на консоль:
 # (8, 'CAPYBARA', 'capybara')
 # (10, 'ARMAGEDDON', 'armageddon')
