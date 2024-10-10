bce479418fca82528784ebcc4898b8e781622fcc
Aleksandr <kostromin63@mail.ru>
2024-10-10 21:21:28 +0300

    Домашнее задание по теме "План написания админ панели"

---
+++ module_14/module_14_4/crud_functions.py
@@ -0,0 +1,81 @@
+# Дополните ранее написанный код для Telegram-бота:
+# Создайте файл crud_functions.py и напишите там следующие функции:
+# initiate_db
+# get_all_products
+import sqlite3 as sq
+
+def connect_to_db():
+    connection = sq.connect('not_telegram.db')
+    cursor = connection.cursor()
+    return (connection, cursor)
+
+def close_connection(connection):
+    connection.commit()
+    connection.close()
+
+def initiate_db():
+    """
+    Создаёт таблицу Products, если она ещё не создана при помощи SQL запроса.
+    Эта таблица должна содержать следующие поля:
+    id - целое число, первичный ключ
+    title(название продукта) - текст (не пустой)
+    description(описание) - тест
+    price(цена) - целое число (не пустой)
+    :return:
+    """
+
+    params_conn = connect_to_db()
+
+    params_conn[1].execute(
+        """
+        CREATE TABLE IF NOT EXISTS Products(
+        id INTEGER PRIMARY KEY,
+        title TEXT NON NULL,
+        description TEXT,
+        price INTEGER NON NULL)
+        """
+    )
+
+    close_connection(params_conn.connection)
+
+def get_all_products():
+    """
+    возвращает все записи из таблицы Products, полученные при помощи SQL запроса.
+    :return:
+    """
+    params_conn = connect_to_db()
+    params_conn[1].execute(
+        """
+        SELECT * FROM Products
+        """
+    )
+
+    return params_conn[1].fetchall()
+
+    close_connection(params_connection.connection)
+
+def add_products(products):
+    params_connection = connect_to_db()
+    for product in products:
+        params_connection.cursor.execute(
+            """INSERT INTO Products (title, description, price) VALUES (?, ?, ?)""",
+            (product[0], product[1], product[2])
+        )
+
+    close_connection(params_connection.connection)
+
+# initiate_db()
+# products = [
+#     ("burgeg", 'Мини Тести', 60),
+#     ("cake", "Бисквитный трайфл", 270),
+#     ("coca-cola", "Просто Cola", 52),
+#     ("donut", 'Пончик "Сладость"', 23),
+#     ("donut witg filing", "Пончик с малиной", 32),
+#     ("ice cream", "Мороженое с ежевикой", 50),
+#     ("lemon juice", "Сок лимона", 250),
+#     ("rolls", "Сальмон ролл", 280)
+# ]
+# add_products(products)
+# print(get_all_products())
+
+
